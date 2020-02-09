2b9296653019543c8bc6d5d952fc6e4904b3c76c
alvindav <alvind@sfu.ca>
2020-02-08 17:58:10 -0800

    autofill scripts added

---
+++ autoLogin.js
@@ -0,0 +1,9 @@
+function enterLogin(){
+  var id = "nickchubb";
+  var password = "insertpasswordhere";
+
+  //setInterval(function(){  }, 3000);
+  document.getElementById('login').value = id;
+  document.getElementById('password').value = password;
+  document.getElementById('submitLogin').click();
+}
---
+++ autoSchool.js
@@ -0,0 +1,8 @@
+function selectSchool(){
+  var userSchool = "uoc";
+
+  document.getElementById('school').value = userSchool;
+  document.getElementById('submitButton').click();
+
+
+}
